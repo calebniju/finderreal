f75e6316c0a747d5ee8858f135c14890d0b21dcb
calebniju <96456763+calebniju@users.noreply.github.com>
2022-06-30 21:35:28 +1200

    Add files via upload

---
+++ 111.js
@@ -0,0 +1,17 @@
+function india(){
+    window.location = "india.html";
+}
+
+function usa(){
+    window.location = "usa.html";
+}
+
+function nz(){
+    window.location = "nz.html";
+}
+
+
+
+
+
+
